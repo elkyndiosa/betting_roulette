df04d65c7b6d6664f274c47bd1477d29ed44febd
Elkyn Gustavo Diosa Martinez <elkyn.diosa@linktic.co>
2021-10-16 16:30:20 -0500

    Create esndpoint for created new roulettes

--- src/functions/roulette/roulette.spec.ts
+++ src/functions/roulette/roulette.spec.ts
@@ -1,6 +1,6 @@
 import { isApiGatewayResponse } from '@src/testUtils/validators';
 import { eventGenerator } from '../../testUtils/eventGenerator';
-import { main } from './roulette';
+import { create } from './roulette';
 import mock = require('./mock.json');
 
 describe('Validating entity roulette', () => {
@@ -8,11 +8,11 @@ describe('Validating entity roulette', () => {
     const context = null;
     const callback = null;
     const event = eventGenerator(mock);
-    const resp = await main(event, context, callback);
+    const resp = await create(event, context, callback);
     expect(resp.statusCode).toBe(200);
     expect(resp).toBeDefined();
     expect(isApiGatewayResponse(resp)).toBe(true);
     const body = JSON.parse(resp.body);
-    expect(body).toEqual({ message: 'Roulette Roulette of test created succesfull', roulette: mock.body });
+    expect(body).toEqual({ message: 'Roulette Roulette of test created succesfull', roulette: { id: mock.body.id } });
   });
 });
